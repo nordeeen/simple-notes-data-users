a959dece0240059ccceda913fa42fc5f0e6c6f1b
nordeeen <ahmadnrdnsah@gmail.com>
2022-07-22 22:51:13 +0700

    adding ErrorModal component

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUsers.module.css";
 
 const AddUsers = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState("");
 
   const addUsersHandler = (e) => {
     e.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      return console.error("Something went wrong!");
+      // return console.error("Something went wrong!");
+      setError({
+        title: "Invalid Username",
+        message: "Please enter a name and age (no-empty values)",
+      });
     }
 
     if (enteredAge < 0) {
-      return console.error("invalid your age");
+      // return console.error("invalid your age");
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age ( < 0 )",
+      });
     }
 
     props.onAddUser(enteredUsername, enteredAge);
@@ -35,6 +45,7 @@ const AddUsers = (props) => {
 
   return (
     <>
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card classCSS={classes.input}>
         <form onSubmit={addUsersHandler}>
           <label htmlFor="username">Username</label>
